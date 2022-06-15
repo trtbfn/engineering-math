7f54313127c313086a6c51c1beb8d3ee387b3c06
trtbfn <halfsapiens@yandex.ru>
2022-06-15 11:06:27 -0400

    initial commit

---
+++ polynomical-division.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+struct X {
+    power int,
+    multiplier int
+};
+
+int main(void) {
+    char[] equation = "^(x, 2) + x + 1"
+    char[] devider = "x + 1"
+
+    
+    
+}
